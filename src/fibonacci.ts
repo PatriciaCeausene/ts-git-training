689da6c19216f62a0ca3c726fd1c4b8e69a9ee87
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 08:59:10 +0100

    handle negative numbers

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,20 +1,37 @@
 export const computeFibonacciNumber = (position: number): number => {
-    if (position === 1 || position === 2) {
+    let i = 1;
+    let j = 1;
+
+    if (position === 0) {
+        return 0;
+    }
+    if (position < 0) {
+        return computeNegativeFibonacci(position);
+    }
+
+    if (position <= 2) {
         return 1;
     }
 
-    let smallFibonacciNumber = 1;
-    let largeFibonacciNumber = 1;
 
     let currentPosition = 2;
     while (currentPosition < position) {
-        const nextFibonacciNumber = smallFibonacciNumber + largeFibonacciNumber;
-        smallFibonacciNumber = largeFibonacciNumber;
-        largeFibonacciNumber = nextFibonacciNumber;
+        const temp = i;
+        i = j;
+        j += temp;
         currentPosition++;
     }
-    return largeFibonacciNumber;
+    return j;
 };
+
+const computeNegativeFibonacci = (position: number): number => {
+    if (position >= 0) {
+        throw new Error(`Position must be less than zero! Received: ${position}.`);
+    }
+    const resultIsNegative = position % 2 === 0;
+    const absoluteResult = computeFibonacciNumber(-position);
+    return resultIsNegative ? absoluteResult * -1 : absoluteResult;
+}
 
 export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
     const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
